08de9488b93debe825c0b393c208ff47f6e58fe5
ccadie883 <christian_cadieux@yahoo.com>
2024-05-11 19:52:10 -0600

    comment

--- operator/pkg/model/translation/gateway-api/translator.go
+++ operator/pkg/model/translation/gateway-api/translator.go
@@ -103,6 +103,9 @@ func getService(resource *model.FullyQualifiedResource, allPorts []uint32, label
 			Protocol: corev1.ProtocolTCP,
 		})
 	}
+	// needed to be set to 0.0.0.0/0 because there is a bug in the existing implementation of
+	// LoadBalancerSourceRanges and when then sourceRanges is set to a value, the set to null, then
+	// service still prevent access. but if the value is 0.0.0.0/0 (instead of null or missing), it works.
 	sourceRanges := []string{"0.0.0.0/0"}
 	if v, ok := annotations[ingestion.SOURCE_RANGE_PREFIX]; ok {
 		sourceRanges = strings.Split(v, ",")
